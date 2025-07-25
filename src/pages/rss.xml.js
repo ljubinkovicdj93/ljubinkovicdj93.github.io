87c215f82986af1925e5f4b4bf6d03334089467b
Djordje Ljubinkovic <djordje.ljubinkovic@bosch.com>
2025-07-25 10:36:12 +0200

    Fix github actions fail and add an RSS feed icon

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import rss from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
-export const get = () =>
-  rss({
+export const get = async () => {
+  return rss({
     // `<title>` field in output xml
     title: "Djordje Ljubinkovic blog",
     // `<description>` field in output xml
-    description: "A startup founder's blog",
+    description: "Aspiring computer vision and machine learning engineer, sharing insights and projects.",
     // base URL for RSS <item> links
     // SITE will use "site" from your project's astro.config.
     site: import.meta.env.SITE,
     // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
-    items: import.meta.glob("./**/*.md"),
+    // Only include blog posts, not project files
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.md")),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
+};
